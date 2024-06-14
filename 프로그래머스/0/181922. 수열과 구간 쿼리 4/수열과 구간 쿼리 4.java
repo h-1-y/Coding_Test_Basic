be602db2c35d917cb14d23f89cb93fc81ffc0260
h-1-y <67308947+h-1-y@users.noreply.github.com>
2024-06-14 15:22:07 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 3.43 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -3,8 +3,7 @@ class Solution {
        
         for ( int i=0; i<queries.length; i++ ) {
 			for ( int j=queries[i][0]; j<=queries[i][1]; j++ ) {
-				if ( j==0 ) arr[j]++;
-				else if ( j%queries[i][2] == 0 ) arr[j]++;
+				if ( j%queries[i][2] == 0 ) arr[j]++;
 			}
 		}
         
