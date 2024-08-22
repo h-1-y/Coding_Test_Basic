5c203cc4285f3e12651f1a9880adcf8b7a469aba
h-1-y <67308947+h-1-y@users.noreply.github.com>
2024-08-22 13:20:03 +0900

    [level 0] Title: 369게임, Time: 7.87 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        
+        for ( String str : (order+"").split("") )
+			if ( str.equals("3") || str.equals("6") || str.equals("9") ) answer++;
+        
+        return answer;
+    }
+}
