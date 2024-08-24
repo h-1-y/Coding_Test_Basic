fb73d22c9e9ae188df5d0321fd5616703e945a53
h-1-y <67308947+h-1-y@users.noreply.github.com>
2024-08-24 11:59:34 +0900

    [level 0] Title: 대문자와 소문자, Time: 31.79 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        for ( char c : my_string.toCharArray() ) 
+			if ( Character.isLowerCase(c) ) answer += (c+"").toUpperCase();
+			else answer += (c+"").toLowerCase();
+        
+        return answer;
+    }
+}
