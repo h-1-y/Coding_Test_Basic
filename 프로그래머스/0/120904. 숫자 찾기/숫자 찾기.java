ea69e3cf060c52f9bbaf248d352f053f8c255948
h-1-y <67308947+h-1-y@users.noreply.github.com>
2024-08-29 12:10:14 +0900

    [level 0] Title: 숫자 찾기, Time: 14.41 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,15 +1,9 @@
 class Solution {
     public int solution(int num, int k) {
         
-        int answer = -1;
-		String [] arr = (num+"").split("");
-		
-		for ( int i=0; i<arr.length; i++ ) 
-			if ( arr[i].equals(k+"") ) {
-				answer = i+1;
-				break;
-			}
+        String str = num+"";
+        int answer = str.indexOf(k+"");
         
-        return answer;
+        return answer == -1 ? -1 : answer + 1;
     }
 }
