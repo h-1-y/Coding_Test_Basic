122d444ab0470e9aecacec444c8efb91d710d806
h-1-y <67308947+h-1-y@users.noreply.github.com>
2025-03-21 14:08:59 +0900

    [level 1] Title: 폰켓몬, Time: 7.80 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        
+        Map<Integer, Integer> map = new HashMap<>();
+		int get = nums.length/2;
+		
+		for ( int i : nums ) {
+			if ( map.get(i) == null ) map.put(i, 1);
+			else map.put(i, map.get(i)+1);
+		}
+        
+        return map.keySet().size() >= get ? get : map.keySet().size();
+        
+    }
+}
