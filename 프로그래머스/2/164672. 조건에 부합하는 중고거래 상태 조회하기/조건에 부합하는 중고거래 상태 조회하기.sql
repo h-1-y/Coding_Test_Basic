11bd44559f73a6aa1ed1872dec8f21c388158989
h-1-y <67308947+h-1-y@users.noreply.github.com>
2026-03-09 15:03:06 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,16 @@
+SELECT
+      BOARD_ID
+    , WRITER_ID
+    , TITLE
+    , PRICE
+    , CASE 
+        WHEN STATUS = 'SALE' THEN '판매중'
+        WHEN STATUS = 'RESERVED' THEN '예약중'
+        ELSE '거래완료'
+      END STATUS
+FROM
+    USED_GOODS_BOARD
+WHERE
+    TO_CHAR(CREATED_DATE, 'YYYYMMDD') = '20221005'
+ORDER BY
+    BOARD_ID DESC
