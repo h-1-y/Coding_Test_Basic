088ebd5ea36ec3c8c5907e72e8f6d15e736fafca
h-1-y <67308947+h-1-y@users.noreply.github.com>
2026-03-17 11:16:48 +0900

    [level 3] Title: 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
+++ 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
@@ -1,33 +1,12 @@
 SELECT
-      A.MONTH
-    , A.CAR_ID
-    , A.CNT AS RECORDS
-FROM
-    (
-        SELECT
               CAR_ID
-            , CAST(TO_CHAR(START_DATE, 'FMMM') AS NUMBER(10)) AS MONTH
-            , COUNT(*) AS CNT
-        FROM
-            CAR_RENTAL_COMPANY_RENTAL_HISTORY
-        WHERE
-            TO_CHAR(START_DATE, 'YYYYMM') >= '202208'
-        AND
-            TO_CHAR(START_DATE, 'YYYYMM') <= '202210'
-        GROUP BY
-            CAR_ID, TO_CHAR(START_DATE, 'FMMM')
-    ) A
-LEFT OUTER JOIN
-    (
-        SELECT
-              CAR_ID
-            , SUM(REC) AS REC
+            , SUM(RECORDS) AS RECORDS
         FROM
             (
                 SELECT
                       CAR_ID
                     , MONTH
-                    , COUNT(*) AS REC
+                    , COUNT(*) AS RECORDS
                 FROM
                     (
                         SELECT
@@ -46,12 +25,4 @@ LEFT OUTER JOIN
         GROUP BY
             CAR_ID
         HAVING
-            SUM(REC) >= 5
-    ) B
-ON
-    A.CAR_ID = B.CAR_ID
-WHERE
-    B.CAR_ID IS NOT NULL
-ORDER BY
-    A.MONTH, A.CAR_ID DESC
-  
+            SUM(RECORDS) >= 5
