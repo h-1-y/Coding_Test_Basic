11b4c314584192e74d11f2bb493cbe73db8f71cc
h-1-y <67308947+h-1-y@users.noreply.github.com>
2026-03-19 15:44:56 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -0,0 +1,20 @@
+SELECT
+      FOOD_TYPE
+    , REST_ID
+    , REST_NAME
+    , FAVORITES
+FROM
+    (
+    SELECT
+          FOOD_TYPE
+        , REST_ID
+        , REST_NAME
+        , FAVORITES
+        , ROW_NUMBER() OVER(PARTITION BY FOOD_TYPE ORDER BY FOOD_TYPE, FAVORITES DESC) AS RN
+    FROM
+        REST_INFO
+    )
+WHERE
+    RN = 1
+ORDER BY
+    FOOD_TYPE DESC
